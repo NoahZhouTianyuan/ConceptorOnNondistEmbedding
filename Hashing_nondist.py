d6f5efb10f97918baff9405d98b7f4a320f526f3
NoahZhouTianyuan <44751366+NoahZhouTianyuan@users.noreply.github.com>
2018-11-18 13:33:13 -0500

    Add files via upload

---
+++ Hashing_nondist.py
@@ -0,0 +1,80 @@
+
+
+import os
+from os.path import join as path_join, exists
+import gzip
+import datetime
+
+
+import numpy as np
+from numpy.linalg import svd
+from scipy.sparse.linalg import svds
+from scipy.sparse import csc_matrix
+from sklearn.feature_extraction import FeatureHasher
+
+
+folder_write = "res3"
+if not exists(folder_write):
+    os.mkdir(folder_write)
+
+now_time = datetime.datetime.now
+start_time = now_time()
+
+IS_WIKI200 = True
+
+wordlist = set()
+if IS_WIKI200:
+    with open("enwiki_vocab_min200.txt") as ff:
+        for line in ff:
+            wordlist.add(line.strip().split(" ")[0])
+
+L_word, data, row_ind, col_ind = [], [], [], []
+with gzip.open("binary-vectors.txt.gz") as ff:
+    for cc, line in enumerate(ff):
+        #if cc > 500: break
+        if cc % 1000 == 0:
+            print(cc, (now_time() - start_time).seconds)
+        line = line.decode("gbk").strip().split(" ")
+        if IS_WIKI200 and (line[0] not in wordlist):
+            continue
+        L_word.append(line[0])
+        for col, ii in enumerate(line[1:]):
+            if float(ii) == 1:
+                row_ind.append(len(L_word) - 1)
+                col_ind.append(col)
+                data.append(1)
+    ncol = len(line) - 1
+
+print(cc, (now_time() - start_time).seconds)
+print("nrow = % s, ncol = % s" % (len(L_word), ncol))
+
+A = csc_matrix((data, (row_ind, col_ind)),
+           shape = (len(L_word), ncol),
+           dtype = float)
+del data, row_ind, col_ind
+
+for k in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]:
+    u, s, vt = svds(A, k = k)
+    print("svd finished", (now_time() - start_time).seconds)
+
+    wiki_tail = "_wiki200" if IS_WIKI200 else ""
+
+    with open(path_join(folder_write, "Nondist_% sD_noSV% s.txt" % (k, wiki_tail)), "w") as ff:
+        ff.write("% s % s\n" % u.shape)
+        for ii, line in zip(L_word, u):
+            ff.write(ii + " " + " ".join(map(str, line)) + "\n")
+    print("written", (now_time() - start_time).seconds)
+
+    with open(path_join(folder_write, "Nondist_% sD_fullSV% s.txt" % (k, wiki_tail)), "w") as ff:
+        ff.write("% s % s\n" % u.shape)
+        for ii, line in zip(L_word, np.matmul(u, np.diag(s))):
+            ff.write(ii + " " + " ".join(map(str, line)) + "\n")
+    print("written", (now_time() - start_time).seconds)
+
+    with open(path_join(folder_write, "Nondist_% sD_halfSV% s.txt" % (k, wiki_tail)), "w") as ff:
+        ff.write("% s % s\n" % u.shape)
+        for ii, line in zip(L_word, np.matmul(u, np.diag(np.sqrt(s)))):
+            ff.write(ii + " " + " ".join(map(str, line)) + "\n")
+    print("written", (now_time() - start_time).seconds)
+
+    print("running finished, running time = ", (now_time() - start_time).seconds)
